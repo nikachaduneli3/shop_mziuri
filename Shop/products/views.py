6718af139062dc2458c0ba194c7704e48a879210
nikachaduneli3 <testig@fds.as>
2024-12-16 19:00:47 +0400

    blabal

--- Shop/products/views.py
+++ Shop/products/views.py
@@ -1,38 +1,39 @@
-from itertools import product
-
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 
 def home(request):
+    product_name = request.GET.get('product_name')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    category = request.GET.get('category')
+    in_stock = request.GET.get('in_stock')
+
     filters = dict()
 
-    product_name = request.GET.get('product_name')
+
     if product_name:
         filters['name__icontains'] = product_name
 
-    min_price = request.GET.get('min_price')
     if min_price:
         filters['price__gt'] = min_price
 
-    max_price = request.GET.get('max_price')
     if max_price:
         filters['price__lt'] = max_price
 
-    address = request.GET.get('address')
-    if address:
-        filters['address__icontains'] = address
+    if in_stock == 'on':
+        filters['stock_qty__gt'] = 0
 
-    category =  request.GET.get('category')
     if category:
-        filters['category_id'] = category
+        filters['category'] = category
 
     products = Product.objects.filter(**filters)
+
     categories = Category.objects.all()
-    return render(request, 'home.html', {'products': products,
-                                                              'categories': categories})
+
+    return render(request, 'home.html', {'products': products, 'categories': categories})
 
 
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
+def product_detail(request):
+    product = get_object_or_404(Product, id=id)
     return render(request, 'product_detail.html', {'product': product})
