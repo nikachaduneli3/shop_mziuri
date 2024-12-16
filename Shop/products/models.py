93ccbd87ab146e3a7f9aa751006843811aa892f6
nikachaduneli3 <testig@fds.as>
2024-12-16 18:57:38 +0400

    added model

--- Shop/products/models.py
+++ Shop/products/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    def __str__(self):
+        return  self.name
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=150, default='Product Name')
+    description = models.TextField(default='')
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
-    views = models.IntegerField(default=0)
-    stock_qty = models.IntegerField(default=0)
+    write_time = models.DateTimeField(auto_now=True)
+    views = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 null=True, related_name='products')
 
+
+    def __str__(self):
+        return  self.name
